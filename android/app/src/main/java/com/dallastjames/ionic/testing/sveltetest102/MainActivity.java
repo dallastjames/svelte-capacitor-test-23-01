e4270fea3f802982ced4995585ec0884c1fd63e7
Dallas James <dallastjames@gmail.com>
2023-01-31 08:58:05 -0600

    init to win it

---
+++ android/app/src/main/java/com/dallastjames/ionic/testing/sveltetest102/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dallastjames.ionic.testing.sveltetest102;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dallastjames.ionic.testing.sveltetest102',
+  appName: 'svelte-cap-test',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
